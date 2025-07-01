2a3061dae5a4b04b45a9ec6ee499ff304664fbed
Dan01011997 <f2016915@pilani.bits-pilani.ac.in>
2025-07-01 21:08:26 +0530

    created board array, comprised of each cell

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Let's do this danish!!");//time start :6::00pm
+
+const cell=function(){
+    let value;
+    function setValue(playerValue){
+        value=playerValue;
+    }
+    function getValue(){
+        return value;
+    }
+    return {getValue,setValue}
+}
+const createBoard=(function()//check baord state, render baord state
+{
+let board=[];//2-d array having cells, 3*3 array, containing 9 cells
+for(let i=0;i<3;i++){
+    board[i]=[];
+    for(let j=0;j<3;j++){
+       board[i][j]=cell();  // board array is a collection of Cell Object instances
+    
+    }
+}
+    console.log(board);
+
+
+})();
+
